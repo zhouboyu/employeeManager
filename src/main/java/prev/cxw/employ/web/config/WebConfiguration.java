4766ef8a151d20f19889f1f7032c233bd06ffb91
boyu <boyu@mogujie.com>
2017-12-01 00:17:59 +0800

    fix

--- src/main/java/prev/cxw/employ/web/config/WebConfiguration.java
+++ src/main/java/prev/cxw/employ/web/config/WebConfiguration.java
@@ -1,7 +1,10 @@
 package prev.cxw.employ.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -11,13 +14,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Time:2017/11/30
  * version:1.0
  */
-//@Configuration
+@Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/assets/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/assets/js/");
+//        super.addResourceHandlers(registry);
+//    }
+
+
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/assets/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/assets/js/");
-        super.addResourceHandlers(registry);
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/error").setViewName("error.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
     }
 }
